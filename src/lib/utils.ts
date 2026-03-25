86566f3c742e83903e7e861d7b4064525e1c6dbb
Brian Tarqui <tarquibrian@gmail.com>
2026-03-25 01:03:23 -0400

    feat: update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,13 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [{ text: ["body", "body-sm", "body-lg"] }],
+    },
+  },
+});
 
 // Utilidad base para combinar clases de Tailwind de forma segura
 export function cn(...inputs: ClassValue[]) {
