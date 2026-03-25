f6a2328e89458c3b18ca96a15a1bc83de86b4f4c
Brian Tarqui <tarquibrian@gmail.com>
2026-03-25 01:01:43 -0400

    feat: update about component

---
+++ src/features/home/components/About/About.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { AnimateOnScroll } from "@/components/ui/AnimateOnScroll";
+
+export const About = () => {
+  return (
+    <section className="w-full">
+      <div className="container max-w-[1512px] mx-auto py-12 px-16">
+        <div className="bg-primary-soft-gray-balance m-full max-w-[1200px] mx-auto p-3 rounded-lg">
+          <AnimateOnScroll variant="slide-up">
+            <div className="flex flex-col gap-4 py-8 max-w-[770px] mx-auto">
+              <header className="flex items-center justify-center gap-6">
+                <span className="text-body font-medium text-primary-orange">NUESTRA MARCA</span>
+                <div className="w-[20px] h-px bg-gray-200"></div>
+                <Image src="/logo-terbol.svg" alt="Terbol" width={200} height={33} />
+              </header>
+              <p className="text-center text-body text-gray-500">Térbol Inspira es la línea premium de vitaminas y nutracéuticos de Térbol, desarrollada con formulaciones respaldadas por evidencia científica. Nacimos para ofrecer productos de alta gama a quienes no se conforman con menos cuando se trata de su salud.</p>
+            </div>
+          </AnimateOnScroll>
+
+          {/* <div className="rounded-lg overflow-hidden bg-primary-black"> */}
+          <AnimateOnScroll variant="fade" delay={0.4} className="aspect-video">
+            <iframe
+              className="w-full h-full rounded-lg"
+              src="https://www.youtube.com/embed/VIDEO_ID"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </AnimateOnScroll>
+          {/* </div> */}
+        </div>
+      </div>
+    </section>
+  );
+}
