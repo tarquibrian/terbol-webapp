718b3cb98989b4c9d75afdfb3d3d52e631fa4cc9
Brian Tarqui <tarquibrian@gmail.com>
2026-03-25 01:00:24 -0400

    feat: udpate button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,44 +3,61 @@ import { cn } from "@/lib/utils";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
+  variant?:
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
   size?: "default" | "sm";
   icon?: React.ReactNode;
   iconPosition?: "left" | "right";
 }
 
+// Clases base para todos los botones
+const BASE_CLASSES =
+  "uppercase inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 min-w-fit";
+
+// Mapeo de variantes
+const VARIANT_CLASSES = {
+  default:
+    "bg-button-orange text-primary-black hover:bg-button-orange-hover",
+  destructive:
+    "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+  outline:
+    "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+  secondary: "bg-primary-black text-white",
+  ghost: "hover:bg-accent hover:text-accent-foreground",
+  link: "text-primary underline-offset-4 hover:underline",
+};
+
+// Mapeo de tamaños
+const SIZE_CLASSES = {
+  default: "h-[48px] px-4 text-body gap-2",
+  sm: "h-[36px] px-3 text-body-sm gap-1.5",
+};
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant = "default", size = "default", icon, iconPosition = "right", children, ...props }, ref) => {
-    // Definimos las clases base
-    const baseClasses =
-      "uppercase inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 min-w-fit";
-
-    // Mapeo de variantes
-    const variantClasses = {
-      default: "bg-button-orange text-primary-foreground hover:bg-button-orange-hover",
-      destructive:
-        "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-      outline:
-        "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-      secondary:
-        "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-      ghost: "hover:bg-accent hover:text-accent-foreground",
-      link: "text-primary underline-offset-4 hover:underline",
-    }[variant];
-
-    // Mapeo de tamaños
-    const sizeClasses = {
-      default: "h-[48px] px-4 text-body gap-2 ",
-      sm: "h-[36px] px-3 text-body-sm gap-1.5",
-    }[size];
-
+  (
+    {
+      className,
+      variant = "default",
+      size = "default",
+      icon,
+      iconPosition = "right",
+      children,
+      ...props
+    },
+    ref
+  ) => {
     return (
       <button
         ref={ref}
         className={cn(
-          baseClasses,
-          variantClasses,
-          sizeClasses,
+          BASE_CLASSES,
+          VARIANT_CLASSES[variant],
+          SIZE_CLASSES[size],
           iconPosition === "right" && "flex-row-reverse",
           className
         )}
