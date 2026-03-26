dd62f1203a5c5fe11b525fc891bf39ca8e9832c6
Brian Tarqui <tarquibrian@gmail.com>
2026-03-25 21:50:40 -0400

    feat: update banner component

--- src/components/layout/Banner/Banner.tsx
+++ src/components/layout/Banner/Banner.tsx
@@ -5,21 +5,21 @@ import { AnimateOnScroll } from "@/components/ui/AnimateOnScroll";
 
 export function Banner() {
   return (
-    <section className="w-full">
-      <div className="container max-w-[1512px] mx-auto py-12 px-6 md:px-16">
+    <section className="wrapper-section">
+      <div className="wrapper-content">
         <div className="flex flex-col lg:grid lg:grid-cols-2 rounded-lg bg-linear-to-b from-[#D2D2D2] to-[#EDEDE8] min-h-[540px] w-full overflow-hidden">
 
           {/* Contenido (Textos + Boton + Contacto) */}
-          <div className="flex flex-col gap-8 lg:gap-10 justify-center items-start w-full px-6 py-10 lg:px-16 lg:py-16">
+          <div className="flex flex-col md:gap-8 lg:gap-10 justify-center items-start w-full px-6 py-10 lg:px-16 lg:py-16">
             <AnimateOnScroll variant="slide-up" className="flex flex-col gap-8 lg:gap-10">
               <div className="flex flex-col gap-4">
                 <h4 className="heading-h3 text-primary">
                   Comienza tu camino hacia el bienestar
                 </h4>
-                <p className="text-body text-gray-500">
+                <p className="text-body-md text-gray-500">
                   Súmate a una comunidad que cree en tu potencial y forma parte de la red de Asesores de Venta Independiente de térbol Inspira donde tu crecimiento personal y profesional van de la mano.
                 </p>
-                <p className="text-body text-gray-500">
+                <p className="text-body-md text-gray-500">
                   Este es tu momento para inspirar, crecer y transformar tu vida.
                 </p>
               </div>
@@ -29,9 +29,14 @@ export function Banner() {
                 size="default"
                 icon={<ArrowRight />}
                 iconPosition="right"
-                className="w-full sm:w-auto"
+                className="w-full sm:w-auto justify-between"
               >
-                REGISTRARSE COMO ASESOR DE VENTAS
+                <span className="hidden sm:block">
+                  REGISTRARSE COMO ASESOR DE VENTAS
+                </span>
+                <span className="block sm:hidden">
+                  REGISTRARSE AHORA
+                </span>
               </Button>
 
               {/* Información de Contacto */}
