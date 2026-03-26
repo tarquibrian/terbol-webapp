9fee5c828c2c4599e24b6eee8af999e9d7403aea
Brian Tarqui <tarquibrian@gmail.com>
2026-03-25 21:50:58 -0400

    feat: update about component

--- src/features/home/components/About/About.tsx
+++ src/features/home/components/About/About.tsx
@@ -3,17 +3,17 @@ import { AnimateOnScroll } from "@/components/ui/AnimateOnScroll";
 
 export const About = () => {
   return (
-    <section className="w-full">
-      <div className="container max-w-[1512px] mx-auto py-12 px-16">
+    <section className="wrapper-section">
+      <div className="wrapper-content">
         <div className="bg-primary-soft-gray-balance m-full max-w-[1200px] mx-auto p-3 rounded-lg">
           <AnimateOnScroll variant="slide-up">
-            <div className="flex flex-col gap-4 py-8 max-w-[770px] mx-auto">
-              <header className="flex items-center justify-center gap-6">
+            <div className="flex flex-col  gap-4 py-4 md:py-8 max-w-[770px] mx-auto">
+              <header className="flex flex-col md:flex-row items-center justify-center gap-3 md:gap-6">
                 <span className="text-body font-medium text-primary-orange">NUESTRA MARCA</span>
                 <div className="w-[20px] h-px bg-gray-200"></div>
                 <Image src="/logo-terbol.svg" alt="Terbol" width={200} height={33} />
               </header>
-              <p className="text-center text-body text-gray-500">Térbol Inspira es la línea premium de vitaminas y nutracéuticos de Térbol, desarrollada con formulaciones respaldadas por evidencia científica. Nacimos para ofrecer productos de alta gama a quienes no se conforman con menos cuando se trata de su salud.</p>
+              <p className="text-center text-body-md text-gray-500">Térbol Inspira es la línea premium de vitaminas y nutracéuticos de Térbol, desarrollada con formulaciones respaldadas por evidencia científica. Nacimos para ofrecer productos de alta gama a quienes no se conforman con menos cuando se trata de su salud.</p>
             </div>
           </AnimateOnScroll>
 
